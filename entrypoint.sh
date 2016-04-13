31b7a0db4a208bd31256990c90e206c962f84645
n10n <ns68751@gmail.com>
2016-04-12 18:23:50 -0700

    added dbport

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed -i 's/dbHost = \"127.0.0.1\"/dbHost = \"'$MONGODB_HOST'\"/' /usr/local/splicious/eval.conf
+sed -i 's/dbPort = \"27017\"/dbPort = \"'$MONGODB_PORT'\"/' /usr/local/splicious/eval.conf
 exec "$@"
