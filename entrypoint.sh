4591ee7f5963c4630a19235eb5c9f44cb4efa20a
n10n <ns68751@gmail.com>
2016-05-23 13:35:17 -0700

    Rename and merged UI and backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-EVAL_DIR=/usr/local/splicious/config
+EVAL_DIR=$W_DIR/splicious/config
+UI_DIR=$W_DIR/frontui
+
 #ENV NODEADMINEMAIL admin@localhost
 #ENV NODEADMINPASS a
+
+#ENV API_HOST "127.0.0.1"
+#ENV API_PORT "9876"
+#ENV RUNB_PORT 9876
+#ENV RUNF_PORT 9000
 
 #DEPLOYMENT_MODE=colocated
 #MONGODB_HOST=127.0.0.1
@@ -14,6 +21,14 @@ EVAL_DIR=/usr/local/splicious/config
 #DSLEPSSERVER=127.0.0.1
 #DSLEPSPORT=5672
 
+## UI
+#update backend server 
+sed -i 's/baseURL: \"http:\/\/localhost:9876\/api\"/baseURL: \"http:\/\/'$API_HOST':'$API_PORT'\/api\"/' $UI_DIR/conf/application.conf
+
+#update running port
+sed -i 's/http.port=9000/http.port='$RUNF_PORT'/' $UI_DIR/runsui.sh
+
+## Backend
 #Node Admin
 sed -i 's/nodeAdminEmail = \"admin@localhost\"/nodeAdminEmail = \"'$NODEADMINEMAIL'\"/' $EVAL_DIR/eval.conf
 sed -i 's/nodeAdminPass = \"a\"/nodeAdminPass = \"'$NODEADMINPASS'\"/' $EVAL_DIR/eval.conf
@@ -21,6 +36,7 @@ sed -i 's/nodeAdminPass = \"a\"/nodeAdminPass = \"'$NODEADMINPASS'\"/' $EVAL_DIR
 #MongdoDB
 sed -i 's/dbHost = \"127.0.0.1\"/dbHost = \"'$MONGODB_HOST'\"/' $EVAL_DIR/eval.conf
 sed -i 's/dbPort = \"27017\"/dbPort = \"'$MONGODB_PORT'\"/' $EVAL_DIR/eval.conf
+
 #DeploymentMode
 sed -i 's/deploymentMode = \"colocated\"/deploymentMode = \"'$DEPLOYMENT_MODE'\"/' $EVAL_DIR/eval.conf
 #DSLCommLinkServer
