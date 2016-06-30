bed75c5754d00ca7f683dfcab14d1ca8b88dbc13
n10n <ns68751@gmail.com>
2016-06-29 20:22:18 -0700

    Update installpkg.sh

--- installpkg.sh
+++ installpkg.sh
@@ -1,11 +1,16 @@
-wget http://apache.claz.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz -O \
-         /usr/lib/apache-maven-3.3.9-bin.tar.gz && \
-wget https://dl.bintray.com/sbt/native-packages/sbt/0.13.11/sbt-0.13.11.tgz -O \
-         /usr/lib/sbt-0.13.11.tgz && \
-cd /usr/lib/ && \
-tar -xzvf apache-maven-3.3.9-bin.tar.gz && \
-tar -xzvf sbt-0.13.11.tgz && \
-    \
-cd /usr/bin && \
-ln -s ../lib/apache-maven-3.3.9/bin/mvn /usr/bin/mvn && \
-ln -s ../lib/sbt/bin/sbt /usr/bin/sbt && \
+if hash mvn 2 > /dev/null; then
+  echo "mvn exists skipping"
+else 
+  wget http://apache.claz.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz -O \
+         /usr/lib/apache-maven-3.3.9-bin.tar.gz
+  tar -xzvf apache-maven-3.3.9-bin.tar.gz -C /usr/lib/
+  cd /usr/bin; ln -s ../lib/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
+fi
+if hash sbt 2> /dev/null; then
+  echo "sbt exists skipping"
+else
+  wget https://dl.bintray.com/sbt/native-packages/sbt/0.13.11/sbt-0.13.11.tgz -O \
+         /usr/lib/sbt-0.13.11.tgz
+  tar -xzvf sbt-0.13.11.tgz -C /usr/lib/ 
+  cd /usr/bin; ln -s ../lib/sbt/bin/sbt /usr/bin/sbt
+fi 
