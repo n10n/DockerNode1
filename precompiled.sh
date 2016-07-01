e469b4ae5599b53c0fcd72787e208e5ed1b64c61
n10n <ns68751@gmail.com>
2016-07-01 15:43:32 -0700

    Update precompiled.sh

--- precompiled.sh
+++ precompiled.sh
@@ -9,6 +9,10 @@
 #  ( cd -P "$(dirname "$source")/" && pwd )
 #}
 #W_DIR="$(findAppHome)"
+if [ -z "$W_DIR" ]; then
+    echo "W_DIR not set, exiting..."
+    exit 1
+fi
 S_DIR=$W_DIR/splicious
 DOC=0
 if [ ! -d $S_DIR ]; then
@@ -35,25 +39,9 @@ if [ ! -d $S_DIR/scripts ]; then
   rm -rf $S_DIR/scripts/.svn
 fi 
 \
-############################## Update jar files
-#LIB_DIR=$S_DIR/lib
-#AJAR=agentservices-store-ia-1.9.5.jar
-#GJAR=gloseval-0.1.jar
-#SJAR=specialK-1.1.8.5.jar
-#wget https://github.com/synereo/compilednode/raw/master/lib/$AJAR -O $LIB_DIR/$AJAR
-#wget https://github.com/synereo/compilednode/raw/master/lib/$GJAR -O $LIB_DIR/$GJAR
-#wget https://github.com/synereo/compilednode/raw/master/lib/$SJAR -O $LIB_DIR/$SJAR
-#LIBUI_DIR=$S_DIR/libui
-#UIAJAR=server.server-1.0.1-assets.jar
-#UIEJAR=server.server-1.0.1-sans-externalized.jar
-#UISJAR=sharedjvm.sharedjvm-0.1-SNAPSHOT.jar
-#wget https://github.com/synereo/compilednode/raw/master/libui/$UIAJAR -O $LIBUI_DIR/$UIAJAR
-#wget https://github.com/synereo/compilednode/raw/master/libui/$UIEJAR -O $LIBUI_DIR/$UIEJAR
-#wget https://github.com/synereo/compilednode/raw/master/libui/$UISJAR -O $LIBUI_DIR/$UISJAR
-##############################
-wget https://github.com/synereo/gloseval/raw/1.0/eval.conf -O $S_DIR/config/eval.conf && \
-wget https://github.com/synereo/gloseval/raw/1.0/log.properties -O $S_DIR/log.properties && \
-wget https://github.com/LivelyGig/ProductWebUI/raw/master/server/src/main/resources/application.conf -O $S_DIR/config/ui.conf && \
+wget https://raw.githubusercontent.com/synereo/gloseval/1.0/eval.conf -O $S_DIR/config/eval.conf && \
+wget https://raw.githubusercontent.com/synereo/gloseval/1.0/log.properties -O $S_DIR/log.properties && \
+wget https://raw.githubusercontent.com/LivelyGig/ProductWebUI/master/server/src/main/resources/application.conf -O $S_DIR/config/ui.conf && \
 \
 cd $S_DIR && \
 ln -fs config/eval.conf eval.conf && \
@@ -61,18 +49,18 @@ ln -fs config/ui.conf ui.conf && \
   \
   if [ 0 -eq $DOC ]; then
     if [ ! -d "$S_DIR/agentui" ]; then
-      wget https://github.com/synereo/dockernode/raw/master/agentui.tar.gz -O $S_DIR/agentui.tar.gz
+      wget https://raw.githubusercontent.com/synereo/dockernode/master/agentui.tar.gz -O $S_DIR/agentui.tar.gz
       cd $S_DIR ; tar -xzvf agentui.tar.gz ; rm -f agentui.tar.gz
     fi 
     if [ ! -f "$S_DIR/bin/scala" ]; then
-      wget https://github.com/synereo/dockernode/raw/master/scalabin.tar.gz -O $S_DIR/scalabin.tar.gz
+      wget https://raw.githubusercontent.com/synereo/dockernode/master/scalabin.tar.gz -O $S_DIR/scalabin.tar.gz
       cd $S_DIR ; tar -xzvf scalabin.tar.gz ; rm -f scalabin.tar.gz
     fi 
     if [ ! -f "$S_DIR/bin/splicious" ]; then
-      wget https://github.com/synereo/dockernode/raw/master/splicious.sh -O $S_DIR/bin/splicious
+      wget https://raw.githubusercontent.com/synereo/dockernode/master/splicious.sh -O $S_DIR/bin/splicious
     fi 
     if [ ! -f "$S_DIR/bin/frontui" ]; then
-      wget https://github.com/synereo/dockernode/raw/master/frontui.sh -O $S_DIR/bin/frontui
+      wget https://raw.githubusercontent.com/synereo/dockernode/master/frontui.sh -O $S_DIR/bin/frontui
     fi 
   fi
 cd $W_DIR/ && \
